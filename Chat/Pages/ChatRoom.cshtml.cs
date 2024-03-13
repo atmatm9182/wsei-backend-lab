1a39e0166310a50e5437e3f942253da57d8b2746
Oleh Kniaziev <oleh.kniaziev@microsoft.wsei.edu.pl>
2024-03-13 10:50:21 +0100

    Added basic chat project

---
+++ Chat/Pages/ChatRoom.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Chat.Pages
+{
+    public class ChatRoomModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
