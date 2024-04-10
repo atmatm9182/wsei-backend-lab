e385945d02244174dede5f80536e826c1f29ec71
Oleh Kniaziev <oleh.kniaziev@microsoft.wsei.edu.pl>
2024-04-10 11:20:54 +0200

    Lab6: Initial changes

---
+++ Infrastructure/EF/Entities/QuizEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace Infrastructure.Entities;
+
+public class QuizEntity
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public ISet<QuizItemEntity> Items { get; set; } = new HashSet<QuizItemEntity>();
+}
---
+++ Infrastructure/Mappers/QuizItemMapper.cs
@@ -0,0 +1,26 @@
+﻿using ApplicationCore.Models;
+using Infrastructure.Entities;
+
+namespace Infrastructure.Mappers;
+
+public class QuizItemMapper
+{
+    public static QuizItem FromEntityToQuizItem(QuizItemEntity entity)
+    {
+        return new QuizItem(
+            entity.Id,
+            entity.Question,
+            entity.IncorrectAnswers.Select(e => e.Answer).ToList(),
+            entity.CorrectAnswer);
+    }
+
+    public static QuizItemEntity FromQuizItemToEntity(QuizItem item)
+    {
+        return new QuizItemEntity()
+        {
+            Id = item.Id,
+            Question = item.Question,
+            CorrectAnswer = item.CorrectAnswer,
+        };
+    }
+}
